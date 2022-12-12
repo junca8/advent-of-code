41cffa14d1e464ce262a8140a72ffcde39c11554
Jonathan Junca <junca8@gmail.com>
2022-12-12 10:45:48 -0700

    day 11

---
+++ 2022/day11/part2.py
@@ -0,0 +1,75 @@
+import math
+
+data = open('input.txt', 'r', encoding='utf-8').read().splitlines()
+
+monkeyArr = []
+countArr = []
+multiplier = 1
+
+class Monkey:
+    def __init__(self, items, operationString, testVal, trueIndex, falseIndex):
+        self.items = items
+        self.operationString = operationString
+        self.testVal = testVal
+        self.trueIndex = trueIndex
+        self.falseIndex = falseIndex
+
+    def addItem(self, item):
+        self.items.append(item)
+
+    def removeItems(self):
+        self.items = []
+    
+    def transformItem(self, item):
+        newValue = item % multiplier
+        operationSplit = self.operationString.split()
+        operationValue = 0
+        if operationSplit[-1] == 'old':
+            operationValue = item
+        else:
+            operationValue = int(operationSplit[-1])
+        new = 0
+        if operationSplit[-2] == '+':
+            new = newValue + operationValue
+        elif operationSplit[-2] == '-':
+            new = newValue - operationValue
+        elif operationSplit[-2] == '*':
+            new = newValue * operationValue
+        elif operationSplit[-2] == '/':
+            new = newValue / operationValue
+        
+        return math.floor(new)
+
+        
+    def checkItem(self, item):
+        if math.remainder(item, self.testVal) == 0:
+            return self.trueIndex
+        else:
+            return self.falseIndex
+
+for i in range(0, len(data), 7):
+    itemsStartIndex = data[i + 1].index(':') + 2
+    itemStrings = data[i + 1][itemsStartIndex:].split(', ')
+    items = [int(i) for i in itemStrings]
+    operationString = data[i + 2]
+    testVal = int(data[i + 3].split()[-1])
+    multiplier *= testVal
+    trueIndex = int(data[i + 4].split()[-1])
+    falseIndex = int(data[i + 5].split()[-1])
+    monkey = Monkey(items, operationString, testVal, trueIndex, falseIndex)
+    monkeyArr.append(monkey)
+    countArr.append(0)
+
+for _ in range(0, 10000):
+    for k in range(0, len(monkeyArr)):
+        monkey = monkeyArr[k]
+        for item in monkey.items:
+            newItem = monkey.transformItem(item)
+            throwIndex = monkey.checkItem(newItem)
+            monkeyArr[throwIndex].addItem(newItem)
+            countArr[k] += 1
+        monkey.removeItems()
+
+countArr.sort()
+
+print(countArr[-1] * countArr[-2])
